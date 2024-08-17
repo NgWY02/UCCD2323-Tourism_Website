0f3237eba5880b97844ac71599f93ec6b5977016
ngweiyu02 <weiyung0091@gmail.com>
2024-08-18 02:26:51 +0800

    add toggle

--- scripts/login.js
+++ scripts/login.js
@@ -47,3 +47,17 @@ function login() {
         errorMessages.textContent = "Incorrect username or password.";
     }
 }
+function togglePassword() {
+    const passwordField = document.getElementById("password");
+    const togglePasswordIcon = document.getElementById("togglePassword").querySelector("i");
+
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+        togglePasswordIcon.classList.remove("fa-eye");
+        togglePasswordIcon.classList.add("fa-eye-slash");
+    } else {
+        passwordField.type = "password";
+        togglePasswordIcon.classList.remove("fa-eye-slash");
+        togglePasswordIcon.classList.add("fa-eye");
+    }
+}
